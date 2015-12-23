ab1ea1e9b11a76749e39b826e145ed168fb73189
Gobinath <slgobinath@gmail.com>
2015-12-23 10:32:48 +0530

    Spatial plugin is added.

--- contramodel/src/main/java/com/aslan/contramodel/service/LocationService.java
+++ contramodel/src/main/java/com/aslan/contramodel/service/LocationService.java
@@ -2,9 +2,20 @@ package com.aslan.contramodel.service;
 
 
 import com.aslan.contra.dto.Location;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.Polygon;
+import org.neo4j.gis.spatial.*;
+import org.neo4j.gis.spatial.pipes.GeoPipeFlow;
+import org.neo4j.gis.spatial.pipes.GeoPipeline;
+import org.neo4j.gis.spatial.rtree.Envelope;
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.kernel.EmbeddedGraphDatabase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 import static org.neo4j.helpers.collection.MapUtil.map;
 
@@ -13,12 +24,17 @@ import static org.neo4j.helpers.collection.MapUtil.map;
  * Created by gobinath on 12/16/15.
  */
 public class LocationService extends Service {
-    private final Logger LOGGER = LoggerFactory.getLogger(LocationService.class);
+    private final static Logger LOGGER = LoggerFactory.getLogger(LocationService.class);
+    private final SpatialDatabaseService spatialDatabaseService;
 
     public LocationService(GraphDatabaseService databaseService) {
         super(databaseService);
+        this.spatialDatabaseService = new SpatialDatabaseService(databaseService);
     }
 
+    public void initialize() {
+
+    }
 
     public Long createLocation(Location location) {
         if (location == null) {
@@ -27,6 +43,8 @@ public class LocationService extends Service {
         Long id = null;
         Long parentId = null;
         Location parent = location.getParent();
+
+        Transaction transaction = databaseService.beginTx();
         if (parent != null) {
             parentId = createLocation(parent);
             id = executeAndReturnID("MATCH (location:Location {code: {location_code}})-[:IN]->(parent:Location) WHERE ID(parent) = {parent_id} RETURN ID(location) as id", "location_code", location.getCode(), "parent_id", parentId);
@@ -43,8 +61,8 @@ public class LocationService extends Service {
             }
             LOGGER.debug("New location {} with id {} is created", location, id);
         }
+        transaction.success();
 
         return id;
     }
-
 }
